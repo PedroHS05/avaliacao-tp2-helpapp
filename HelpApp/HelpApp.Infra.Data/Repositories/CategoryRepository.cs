2fcdc3e2b87f13bfb6d6f265520abe4aa7bd5a59
PedroHS05 <pedro.scabelo2b2@gmail.com>
2025-04-22 12:38:08 -0300

    Inicio da atividade avaliativa

--- HelpApp/HelpApp.Infra.Data/Repositories/CategoryRepository.cs
+++ HelpApp/HelpApp.Infra.Data/Repositories/CategoryRepository.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HelpApp.Domain.Entities;
+using HelpApp.Domain.Interfaces;
+using HelpApp.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelpApp.Infra.Data.Repositories
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _context.Categories.Include(c => c.Products).ToListAsync();
+        }
+
+        public async Task<Category> GetById(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return null;
+            }
+            return await _context.Categories.Include(c => c.Products).FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            var existingCategory = await _context.Categories.FindAsync(category.Id);
+            if (existingCategory == null)
+            {
+                return null;
+            }
+
+            _context.Entry(existingCategory).CurrentValues.SetValues(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Remove(Category category)
+        {
+            var existingCategory = await _context.Categories.Include(c => category.Products).FirstOrDefaultAsync(c => c.Id == category.Id);
+            if (existingCategory != null)
+            {
+                return null;
+            }
+
+            if (existingCategory.Products?.Any() == true)
+            {
+                _context.Products.RemoveRange(existingCategory.Products);
+            }
+
+            _context.Categories.Remove(existingCategory);
+            await _context.SaveChangesAsync();
+            return category;
+        }
     }
 }
--- HelpApp/HelpApp.Infra.Data/Repositories/ProductRepository.cs
+++ HelpApp/HelpApp.Infra.Data/Repositories/ProductRepository.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HelpApp.Domain.Entities;
+using HelpApp.Domain.Interfaces;
+using HelpApp.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelpApp.Infra.Data.Repositories
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.Include(p => p.Category).ToListAsync();
+        }
+
+        public async Task<Product> GetById(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return null;
+            }
+
+            return await _context.Products.Include(p => p.Category).FirstOrDefaultAsync(p => p.Id == id.Value);
+        }   
+        
+        public async Task<Product> Create(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> Update(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> Remove(Product product)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+
     }
 }
