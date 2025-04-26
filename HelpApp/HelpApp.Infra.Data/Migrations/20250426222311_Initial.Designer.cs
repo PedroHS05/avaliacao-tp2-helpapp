16c1cf6a45efe73e0d0f056198de38bf9a9240bf
PedroHS05 <pedro.scabelo2b2@gmail.com>
2025-04-26 19:31:39 -0300

    Atividade Finalizada

--- HelpApp/HelpApp.Infra.Data/Migrations/20250426222311_Initial.Designer.cs
+++ HelpApp/HelpApp.Infra.Data/Migrations/20250426222311_Initial.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HelpApp.Infra.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250426024422_Initial")]
+    [Migration("20250426222311_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
